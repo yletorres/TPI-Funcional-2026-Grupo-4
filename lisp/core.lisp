5abde7ae2c377df687b7f6f81cdd0edcf7ffe28a
yletorres <torres.yle@gmail.com>
2026-05-29 17:22:41 -0300

    Añadi core.lisp

---
+++ lisp/core.lisp
@@ -0,0 +1,14 @@
+; ########### SISTEMA DE SEMÁFOROS INTELIGENTES ###########
+
+;; ========================================================
+;; FUNCIÓN: Transicion
+;; NATURALEZA: 
+;; ESTRATEGIA: 
+;; IMPACTO: 
+;; ========================================================
+
+(defun transicion (color-actual cambiar-a)
+	(list color-actual )
+	;definir que es una transicion invalida y una valida para resolver.
+	;Devuelve una lista con color actual y un string
+)
