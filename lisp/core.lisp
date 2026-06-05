bbbe2dd089d0ab4601cb5d0ef988055277bb46ef
yletorres <torres.yle@gmail.com>
2026-06-05 20:33:11 -0300

    Borrador de la Bitacora de Depuracion

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -12,3 +12,23 @@
 	;definir que es una transicion invalida y una valida para resolver.
 	;Devuelve una lista con color actual y un string
 )
+
+
+;; ========================================================
+;; FUNCIÓN: Transicion
+;; NATURALEZA: 
+;; ESTRATEGIA: 
+;; IMPACTO: 
+;; ========================================================
+
+(defun timer (tiempo-actual)
+	;se asume que 0 es el inicio en rojo?
+)
+
+;; ========================================================
+;; FUNCIÓN: Transicion
+;; NATURALEZA: 
+;; ESTRATEGIA: 
+;; IMPACTO: 
+;; ========================================================
+
